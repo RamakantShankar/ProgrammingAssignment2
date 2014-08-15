76e13ff0b0fa6cad25b0789be6b91b9e7fdd86c4
Ramakant Shankar <ramakantshankar@yahoo.co.in>
2014-08-15 11:52:46 +0530

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
+
+## -----------------------------------------------------------------
 ## TThis function creates a special "matrix" object that can cache its inverse
 ## hence getting faster results for repitative input from cached value## The 
 ## This funciton creates a vector, which variable value can be used in subsequent function calls
+## -----------------------------------------------------------------
 
 makeCacheMatrix <- function(x=matrix()) {
   ## creating functions which will cache the values
@@ -16,15 +19,17 @@ makeCacheMatrix <- function(x=matrix()) {
 }
 
 
+## -----------------------------------------------------------------
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), 
-## then the cachesolve should retrieve the inverse from the cache.
+## then the cacheSolve should retrieve the inverse from the cache.
+## -----------------------------------------------------------------
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Returns a matrix that is the inverse of 'x'
   m <- x$getSolve()
   if(! is.null(m)){
-    print("getting cached Matrix")
+    print("getting cached Matrix")  # Prints message on screen whenever cache is used
     return(m)
   }
   
@@ -34,14 +39,16 @@ cacheSolve <- function(x, ...) {
   m
 }
 
-# Executing the function
-# a <- makeCacheMatrix(matrix(4:7,2))
-# a$get()
-# a$getSolve()
-# a$set(matrix(5:8,2))
-# a$get()
-# cacheSolve(a)
-# cacheSolve(a)
-# a$getSolve()
-# b = a$getSolve()
-# a$get() %*% b
+## -----------------------------------------------------------------
+## example commands for Executing the function
+## -----------------------------------------------------------------
+## a <- makeCacheMatrix(matrix(4:7,2))
+## a$get()
+## a$getSolve()
+## a$set(matrix(5:8,2))
+## a$get()
+## cacheSolve(a)      # a new matrix is provided hence it will be calculated
+## cacheSolve(a)      # Same matrix is the Input cache should be used - "Getting cached matrix" - message should appear
+## a$getSolve()
+## b = a$getSolve()
+## a$get() %*% b
